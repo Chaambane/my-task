7183bbf763084e736637dd29ff63efbe9e902b8a
FAIDINE <mohamed.faidine@live.fr>
2021-10-07 09:32:46 +0200

    feat(MyTasksManager) create my task manager file

---
+++ src/containers/MyTasksManager/MyTasksManager.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class MyTasksManager extends Component {
+    render() {
+        return (
+            <main>
+                
+            </main>
+        )
+    };
+}
+
+export default MyTasksManager;
